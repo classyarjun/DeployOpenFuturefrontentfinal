8e0f97d22b81ce3ebb05829d39fc51e940c8541d
Arjun Rajput <arjunrajput7531@gmail.com>
2024-12-03 00:34:16 +0530

    images are aded

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import SwiperCore, { Pagination } from 'swiper';
+// Swiper modules ko register karna
+SwiperCore.use([Pagination]);
 
 @Component({
   selector: 'app-testing',
   templateUrl: './testing.component.html',
   styleUrls: ['./testing.component.css'],
 })
-  export class TestingComponent{
 
-
+export class TestingComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const swiper = new SwiperCore('.mySwiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      spaceBetween: 20,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
 }
-
-
-
